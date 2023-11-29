ab9df7e1974be2128eeb8f4d5998f45ac7a7d3fc
Ninanwt <ninanwt19@gmail.com>
2023-11-29 09:01:05 +0700

    semangattt

--- js/Script.js
+++ js/Script.js
@@ -12,3 +12,40 @@ window.onscroll = () =>{
 }
 
 
+// CONTACT US
+function submitForm() {
+  // Mendapatkan nilai input dari formulir
+  const name = document.getElementById('name').value;
+  const message = document.getElementById('message').value;
+
+  // Memeriksa apakah kolom formulir telah diisi
+  if (name.trim() === '' || message.trim() === '') {
+      // Menampilkan notifikasi jika formulir belum diisi
+      alert('Harap isi semua kolom formulir terlebih dahulu.');
+  } else {
+      // Lakukan sesuatu dengan data formulir (simpan, kirim, dll.)
+
+      // Tampilkan popup terima kasih
+      document.getElementById('confirmationPopup').style.display = 'flex';
+
+      // Reset formulir
+      resetForm();
+  }
+}
+
+function resetForm() {
+  // Reset nilai formulir
+  document.getElementById('contactForm').reset();
+}
+
+function cancelForm() {
+  // Reset dan tutup formulir
+  resetForm();
+}
+
+function closePopup() {
+  // Tutup popup terima kasih
+  document.getElementById('confirmationPopup').style.display = 'none';
+}
+
+
