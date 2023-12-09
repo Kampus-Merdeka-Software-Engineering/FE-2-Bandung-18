8329f1c29175be233c11134af55eaddabe3c891c
Ninanwt <ninanwt19@gmail.com>
2023-12-09 21:17:02 +0700

    fixed auto slider

--- js/home.js
+++ js/home.js
@@ -3,7 +3,7 @@ var swiper = new Swiper(".home-slider", {
   spaceBetween: 30,
   centeredSlides: true,
   autoplay: {
-    delay: 5500,
+    delay: 3500,
     disableOnInteraction: false,
   },
   pagination: {
