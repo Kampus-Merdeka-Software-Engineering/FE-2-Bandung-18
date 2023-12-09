7687e7c9e5bb6a7673cc1f2e65ca8cf565b804d7
Ninanwt <ninanwt19@gmail.com>
2023-12-09 21:09:52 +0700

    benerin auto slider

--- js/home.js
+++ js/home.js
@@ -1,16 +1,15 @@
 // AUTO SLIDER
 var swiper = new Swiper(".home-slider", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 8000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true
-    },
-      loop:true,
-    });
-
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop:true,
+});
 
